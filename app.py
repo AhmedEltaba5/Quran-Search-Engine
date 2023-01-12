884e39c997ef93316a9bef2f6a3fbb64d5f2de1d
unknown <Eltabakh36@gmail.com>
2023-01-12 18:18:08 +0200

    update api

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from typing import Dict, List
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request, Form
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 from quran_search import SearchEngine
 
 app = FastAPI()
+templates = Jinja2Templates(directory='templates')
+app.mount("/templates", StaticFiles(directory="templates"), name="templates")
+
 search_engine = SearchEngine()
 
 class QueryText(BaseModel):
@@ -27,6 +33,18 @@ async def predict(query_text: QueryText):
     except Exception as e:
         raise HTTPException(status_code=503, detail=str(e))
 
+
+@app.get("/search")
+def form_post(request: Request):
+    return templates.TemplateResponse('index.html', context={'request': request})
+
+@app.post("/search")
+async def form_post(request: Request, query_txt: str = Form(...)):
+    predictions = search_engine.run_search(query_txt)
+
+    return templates.TemplateResponse('results.html', context={'request': request,
+                                                             'query': query_txt,
+                                                             'predictions': predictions})
 
 @app.get("/")
 def home():
